5443ee9cd582d258f81f43b7ef1f5d3f4c1ea33b
Kamal Preet Singh <kamalpreetsingh025@gmail.com>
2023-02-14 15:02:53 -0500

    Exception Handling using Filter Attribute

--- DineDeck.Api/Program.cs
+++ DineDeck.Api/Program.cs
@@ -1,3 +1,4 @@
+using DineDeck.Api.Filters;
 using DineDeck.Api.Middleware;
 using DineDeck.Application;
 using DineDeck.Infrastructure;
@@ -7,12 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services
     .AddApplication()
     .AddInfrastructure(builder.Configuration);
-    builder.Services.AddControllers();
+    builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
 }
 
 var app = builder.Build();
 {
-    app.UseMiddleware<ErrorHandlingMiddleware>();
+    // app.UseMiddleware<ErrorHandlingMiddleware>();
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
